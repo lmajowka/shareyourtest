88304d5b4fa3f9f4c8ca019a92640649bb1e48de
Leo <leo.majowka@gmail.com>
2014-09-18 17:28:59 +0300

    add signout verifications to auth spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,8 +24,6 @@ RSpec.describe "Authentication", :type => :request do
           click_button "Sign in"
         end
 
-        it { should have_title(user.name) }
-        it { should have_link('Profile',     href: user_path(user)) }
         it { should have_link('Sign out',    href: signout_path) }
         it { should_not have_link('Sign in', href: signin_path) }
       end
