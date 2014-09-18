dad4fffa1a46ac09a1d8ba1bd7b8e035531fcc66
Leo <leo.majowka@gmail.com>
2014-09-18 19:32:26 +0300

    change welcome message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Share your Test!"
       redirect_to user
     else
       flash[:error] = 'Invalid email/password combination'
