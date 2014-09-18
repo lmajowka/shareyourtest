94a258ba4806a6eb45f972d3957a9e82aef8c278
Leo <leo.majowka@gmail.com>
2014-09-18 09:15:24 +0300

    Fix its rspec 3.0

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
  require 'rails_helper'
+ require 'rspec/its'
 
 RSpec.describe User, :type => :model do
     before { @user = User.new(email: "user@example.com", password:"123456", password_confirmation:"123456") }
@@ -6,7 +7,15 @@ RSpec.describe User, :type => :model do
     subject { @user }
 
     it { should respond_to(:email) }
+    it { should respond_to(:remember_token) }
+    it { should respond_to(:authenticate) }
+
     it { should be_valid }
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
 
     describe "when email address is already taken" do
       before do
