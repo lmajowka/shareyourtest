2b88c8e55398d1fa8c11976b15cead2414c0cffa
Leo <leo.majowka@gmail.com>
2014-09-21 08:59:58 +0300

    question model validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ActiveRecord::Base
   belongs_to :test
+
+  validates :content, presence:   true
+  validates :answer, presence:   true, numericality: true
+  validates_presence_of :test
+
 end
