f47e8f5e6f9d322278675049e1813d7e6f565901
Leo <leo.majowka@gmail.com>
2014-09-29 18:12:03 +0300

    secure secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Shareyourtest::Application.config.secret_key_base = '5b403a868eb5b3af21a34ca587439865bed8e961b3f09b45bbef856fb15f3a799f191c9f634ed08853d45fbb4fc245cbebb489c10b0e95552578701173985d21'
+Shareyourtest::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || SecureRandom.hex(64)
+
