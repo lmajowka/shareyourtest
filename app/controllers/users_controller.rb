6d1468f7e28c0233ed5b35e4925936ba1383ff31
lmajowka <leo.majowka@gmail.com>
2014-10-14 09:03:38 +0300

    fix user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :name)
   end
 
 end
