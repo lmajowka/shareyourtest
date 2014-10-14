da7c58b39a976c0c7f451acfa679b78da3f86f37
lmajowka <leo.majowka@gmail.com>
2014-10-14 09:15:12 +0300

    fix test answers

--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -4,5 +4,5 @@ class UserAnswer < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
 
-  scope :answers_for, -> (purchase) {where(purchase_id: purchase.id)}
+  scope :answers_for, -> (purchase) {where(purchase_id: purchase.id).order(created_at: :asc)}
 end
