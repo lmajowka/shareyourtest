554411a7f0b3eeaa3a3ce4c8ee11b8b9f305a8c4
lmajowka <leo.majowka@gmail.com>
2014-10-16 20:15:01 +0300

    refactor test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -42,8 +42,10 @@ class TestsController < ApplicationController
     else
       @answered_purchases = []
     end
-    @average_rating = @test.average_rating
-    @number_of_ratings =  "#{@test.number_of_ratings} #{'rating'.pluralize(@test.number_of_ratings)}"
+    @average_rating = @test.average_rating 
+    @number_of_ratings =  @test.pluralize_number_of_ratings
+    @sample_question = @test.questions.first
+
     respond_to do |format|
       format.html
       format.xml  { render :xml => @test }
