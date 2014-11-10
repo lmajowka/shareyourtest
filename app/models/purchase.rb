345458f70e773b429f5516c19fe6af2f3265ab1a
leo <leo.majowka@gmail.com>
2014-11-10 09:20:56 +0200

    fix bug in #calculate_performance

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -13,10 +13,10 @@ class Purchase < ActiveRecord::Base
 
 
   def calculate_performance
-  	total_user_answers = UserAnswer.where(purchase_id:id).count.to_f
+  	total_answers = Question.where(exam_id:exam_id).count.to_f
     right_user_answers = UserAnswer.where(purchase_id:id, status: 'right').count.to_f
-    return 0 if total_user_answers == 0
-    self.performance =  (right_user_answers/total_user_answers).round(2)
+    self.performance = 0 if total_answers == 0
+    self.performance =  (right_user_answers/total_answers).round(2)
     ranking = Ranking.find_or_create_by(user_id: self.user_id, exam_id: self.exam_id)
     if ranking.performance.nil?
       ranking.update!(performance: self.performance)
