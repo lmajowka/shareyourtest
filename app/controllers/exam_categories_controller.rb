c8c6f94f918d600e42020977cdbf6ee9d89b35d1
leo <leo.majowka@gmail.com>
2014-11-20 08:47:17 +0200

    Add question sample to exam categories pages

--- app/controllers/exam_categories_controller.rb
+++ app/controllers/exam_categories_controller.rb
@@ -2,6 +2,7 @@ class ExamCategoriesController < ApplicationController
   def show
     @exam_category = ExamCategory.find_by_permalink params[:id]
     @tests = Exam.where(exam_category_id: @exam_category.id)
+    @sample_question = @tests.first.questions.first
   end
 
   def new
