8738d5aae1a17cd64a309eea1abcc9c92bcf29ed
leo <leo.majowka@gmail.com>
2014-11-25 22:51:02 +0200

    remove fastly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Shareyourtest::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['FASTLY_CDN_URL']
-  config.static_cache_control = 'public, s-maxage=2592000, maxage=86400'
+  #config.action_controller.asset_host = ENV['FASTLY_CDN_URL']
+  config.static_cache_control = 'public, s-maxage=2592000, maxage=6400'
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
