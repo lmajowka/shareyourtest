807014fc5b67d76283280fb276589eedd3f237e6
leo <leo.majowka@gmail.com>
2014-11-28 08:59:57 +0200

    fix new exams categories

--- app/controllers/exam_categories_controller.rb
+++ app/controllers/exam_categories_controller.rb
@@ -2,7 +2,7 @@ class ExamCategoriesController < ApplicationController
   def show
     @exam_category = ExamCategory.find_by_permalink params[:id]
     @tests = Exam.where(exam_category_id: @exam_category.id)
-    @sample_question = @tests.first.questions.first
+    @sample_question = @tests.first.questions.first if @tests.size > 0
     @embeddable = Embeddable.new
   end
 
