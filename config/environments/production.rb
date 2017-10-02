1a2371ca1346ebecb4d0c8b8127c2f151a15284a
leo <leo.majowka@gmail.com>
2017-10-02 21:00:57 +0300

    remove bootsy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Shareyourtest::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
