c761d9c40161eac1b2ae87ff42cfe9544ed2df1a
leo <leo.majowka@gmail.com>
2017-10-02 20:43:34 +0300

    remove config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,7 @@ Shareyourtest::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
