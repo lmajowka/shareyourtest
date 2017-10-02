73a19cf9e8dec9fd5d971bee23a740033f88f6e0
leo <leo.majowka@gmail.com>
2017-10-02 20:24:57 +0300

    remove old config

--- config/application.rb
+++ config/application.rb
@@ -8,31 +8,5 @@ Bundler.require(*Rails.groups)
 
 module Shareyourtest
   class Application < Rails::Application
-
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-
-    # config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
-
-    config.action_mailer.delivery_method   = :postmark
-    config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
